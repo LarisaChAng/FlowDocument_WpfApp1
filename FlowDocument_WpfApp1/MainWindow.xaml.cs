da51929e617270a8004c4f4a032984de32ad4b0a
LarisaChAng <LChS81@mail.ru>
2022-12-22 22:57:34 +0800

    Добавьте файлы проекта.

---
+++ FlowDocument_WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+using System.Windows.Markup;
+
+namespace FlowDocument_WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            docReader.ClearValue(FlowDocumentReader.DocumentProperty);
+        }
+
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    using (FileStream fs = File.Open("1.xaml", FileMode.Create))
+        //    {
+        //        XamlWriter.Save(docReader.Document, fs);
+        //    }
+        //}
+
+        //private void Button_Click_2(object sender, RoutedEventArgs e)
+        //{
+        //    using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+        //    {
+        //        docReader.Document = XamlReader.Load(fs) as FlowDocument;
+        //    }
+        //} 
+    }
+}
+
+
